dacb334454ed99be26d54f2539a2ab758f3e5130
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-11-04 20:20:15 +0100

    fix: increase spacing bewteen obstacles (easier)

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var obstacle_scene : PackedScene
-@export var obstacle_speed := 6
+@export var player_speed: float = 7.0
 @export var obstacle_y_min := 300.0
 @export var obstacle_y_max := 800.0
 
@@ -29,7 +29,7 @@ var score: int = 0:
 @onready var States = player.States
 
 func _ready() -> void:
-	timer.wait_time = 6.0/obstacle_speed
+	timer.wait_time = 7.0/player_speed
 	timer.timeout.connect(spawn_obstacle)
 	pause_button.pressed.connect(_on_pause_pressed)
 	player.state_changed.connect(_on_player_state_changed)
@@ -46,7 +46,7 @@ func _on_pause_pressed() -> void:
 func _physics_process(delta: float) -> void:
 	ground_body.position.x = player.position.x
 	if player.state in [player.States.READY, player.States.FLYING]:
-		player.position.x += obstacle_speed*delta*60
+		player.position.x += player_speed*delta*60
 		camera.position.x = player.position.x + camera_player_offset
 	if player.state == player.States.FLYING:
 		# Check if the player passed an obstacle
