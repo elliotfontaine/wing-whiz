7afbcf49ecb8283b11d87418a7162445860a1231
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-11-11 23:04:42 +0100

    feat(ui): add juice for new best score

--- scenes/game_over_menu.gd
+++ scenes/game_over_menu.gd
@@ -4,6 +4,9 @@ const TWITTER_SHARE_URL = "https://twitter.com/intent/tweet?text="
 var TWITTER_SHARE_TEMPLATE = "✨ I achieved a score of %s in Wing Whiz ! ✨\n
 Download or Play the game at{itch}".format({"itch": Globals.ITCHIO_URL})
 
+var BEST_SCORE_TEMPLATE = "[right]%s[/right]"
+var NEW_BEST_SCORE_TEMPLATE = "[right][rainbow sat=0.6 val=0.9][wave amp=15.0 freq=7.0]New [/wave][/rainbow]%s[/right]"
+
 #TODO: replace with other medals when assets are made
 var medal_textures := {
 	"bronze": preload("res://assets/ui/medals/bronze_2.png"),
@@ -25,7 +28,7 @@ var menu_scene: String = "res://scenes/home.tscn"
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 @onready var medal: TextureRect = %Medal
 @onready var score_label: Label = %Score
-@onready var best_label: Label = %Best
+@onready var best_label := %Best
 @onready var retry_button: Button = %RetryButton
 @onready var home_button: Button = %HomeButton
 @onready var share_button: Button = %ShareButton
@@ -37,7 +40,7 @@ func _ready() -> void:
 	home_button.pressed.connect(SceneChanger.change_to.bind(menu_scene))
 
 func appear(new_score: int, previous_best_score: int) -> void:
-	best_label.text = str(previous_best_score)
+	best_label.text = BEST_SCORE_TEMPLATE % previous_best_score
 	share_button.pressed.connect(_on_share_pressed.bind(new_score))
 	animation_player.play("appear")
 	var score_tween := score_label.create_tween().set_trans(Tween.TRANS_QUAD)
@@ -49,10 +52,10 @@ func appear(new_score: int, previous_best_score: int) -> void:
 	update_medal(new_score)
 	if new_score > previous_best_score:
 		var record_tween := best_label.create_tween().set_trans(Tween.TRANS_QUAD)
-		best_label.text = str(new_score)
+		best_label.text = NEW_BEST_SCORE_TEMPLATE % new_score
 		best_label.pivot_offset = best_label.size / 2
 		best_label.scale *= 5
-		record_tween.tween_property(best_label, "scale", best_label.scale / 5, 0.3)
+		record_tween.tween_property(best_label, "scale", best_label.scale / 5, 1)
 
 func _update_score(new_score: int):
 	score_label.text = str(new_score)
