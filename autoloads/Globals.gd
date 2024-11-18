095b0f8fd82bdbfe8bccc42a34ef5ad62441d008
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-11-18 15:50:32 +0100

    refactor: use project settings to find target height/width

--- autoloads/Globals.gd
+++ autoloads/Globals.gd
@@ -1,4 +1,11 @@
 extends Node
 
+
+const GAME_VERSION := "0.1.0"
+const SAVEFILE_VERSION := "0.1.0"
 const GITHUB_URL := "https://github.com/elliotfontaine/wing-whiz"
 const ITCHIO_URL := "placeholder_itch.io_url"
+
+var GAME_NAME: String = ProjectSettings.get_setting("application/config/name")
+var TARGET_HEIGHT: int = ProjectSettings.get_setting("display/window/size/viewport_height")
+var TARGET_WIDTH: int = ProjectSettings.get_setting("display/window/size/viewport_width")
--- autoloads/ResponsiveUI.gd
+++ autoloads/ResponsiveUI.gd
@@ -2,9 +2,6 @@ extends Node
 
 signal ratio_changed(new_ratio)
 
-const TARGET_WIDTH: float = 960.0
-const TARGET_HEIGHT: float = 1280.0
-
 var ratio: float = 1.0
 
 func _ready() -> void:
@@ -15,6 +12,7 @@ func _on_viewport_resized() -> void:
     _update_ratio()
 
 func _update_ratio() -> void:
-    ratio = (DisplayServer.window_get_size().x / TARGET_WIDTH) / (DisplayServer.window_get_size().y / TARGET_HEIGHT)
+    ratio = (DisplayServer.window_get_size().x / float(Globals.TARGET_WIDTH)) \
+            / (DisplayServer.window_get_size().y / float(Globals.TARGET_HEIGHT))
     ratio_changed.emit(ratio)
     print_debug("New viewport: " + str(DisplayServer.window_get_size()) + ". Ratio: " + str(ratio))
