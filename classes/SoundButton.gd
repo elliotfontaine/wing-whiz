2b15fe0b9460c7dd3733618280fb1b14ba6751cc
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-11-19 20:08:28 +0100

    fix(ui): use pointing hand cursor shape on all buttons

--- classes/SoundButton.gd
+++ classes/SoundButton.gd
@@ -6,6 +6,7 @@ class_name SoundButton
 @onready var sound_player := AudioStreamPlayer.new()
 
 func _ready():
+	mouse_default_cursor_shape = CursorShape.CURSOR_POINTING_HAND
 	sound_player.stream = sound
 	sound_player.bus = "SFX"
 	add_child(sound_player)
