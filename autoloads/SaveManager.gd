61abb5f08ea407c544b48497bcb3b6d13dab2efd
Elliot Fontaine <92150839+elliotfontaine@users.noreply.github.com>
2024-11-20 20:03:23 +0100

    feat(audio): save audio settings to file

--- autoloads/SaveManager.gd
+++ autoloads/SaveManager.gd
@@ -3,20 +3,36 @@ extends Node
 var save_path: String = "user://save.dat"
 var save = ConfigFile.new()
 
-# Use setters and getters to access/change any save file item.
+# Score
 var best_score: int:
 	set(new_record):
 		save.set_value("score", "best_score", new_record)
 		save.save(save_path)
-	get: 
-		return save.get_value("score", "best_score")
-# Another exemple:
-#var player_skin: int:
-	#set(new_skin):
-		#save.set_value("misc", "player_skin", new_skin)
-		#save.save(save_path)
-	#get: 
-		#return save.get_value("misc", "player_skin")
+	get:
+		return save.get_value("score", "best_score", 0)
+
+# Settings
+var volume_global: float:
+	set(new_volume):
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Global"), linear_to_db(new_volume))
+		save.set_value("settings", "volume_global", new_volume)
+		save.save(save_path)
+	get:
+		return save.get_value("settings", "volume_global", 1.0)
+var volume_music: float:
+	set(new_volume):
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(new_volume))
+		save.set_value("settings", "volume_music", new_volume)
+		save.save(save_path)
+	get:
+		return save.get_value("settings", "volume_music", 1.0)
+var volume_sfx: float:
+	set(new_volume):
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(new_volume))
+		save.set_value("settings", "volume_sfx", new_volume)
+		save.save(save_path)
+	get:
+		return save.get_value("settings", "volume_sfx", 1.0)
 
 func _ready() -> void:
 	if !FileAccess.file_exists(save_path):
@@ -24,8 +40,17 @@ func _ready() -> void:
 		_create_default_save()
 	else:
 		print("Existing save file at: ", save_path)
+	_load_save()
+
+func _load_save() -> void:
 	save.load(save_path)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Global"), linear_to_db(volume_global))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(volume_music))
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), linear_to_db(volume_sfx))
 
 func _create_default_save() -> void:
 	save.set_value("score", "best_score", 0)
+	save.set_value("settings", "volume_global", 1.0)
+	save.set_value("settings", "volume_music", 1.0)
+	save.set_value("settings", "volume_sfx", 1.0)
 	save.save(save_path)
---
+++ classes/VolumeSlider.gd
@@ -0,0 +1,52 @@
+@tool
+extends HSlider
+class_name VolumeSlider
+
+enum AudioBus {}
+@export var audio_bus: AudioBus = 0 as AudioBus
+@onready var bus_name: String = AudioServer.get_bus_name(audio_bus)
+
+func _ready() -> void:
+    value_changed.connect(_on_value_changed)
+    value = SaveManager.get("volume_" + bus_name.to_lower())
+
+func _on_value_changed(new_value: float) -> void:
+    SaveManager.set("volume_" + bus_name.to_lower(), new_value)
+
+func _validate_property(property: Dictionary):
+    if property.name == "audio_bus":
+        var busNumber = AudioServer.bus_count
+        var options = ""
+        for i in busNumber:
+            if i > 0:
+                options += ","
+            var busName = AudioServer.get_bus_name(i)
+            options += busName
+        property.hint_string = options
+
+## The following is portable (doesn't depend on the SaveManager autoload)
+
+# @tool
+# extends HSlider
+#
+# enum AudioBus {}
+# @export var audio_bus: AudioBus = 0 as AudioBus
+# @onready var bus_name: String = AudioServer.get_bus_name(audio_bus)
+#
+# func _ready() -> void:
+#     value_changed.connect(_on_value_changed)
+#     value = db_to_linear(AudioServer.get_bus_volume_db(audio_bus))
+#
+# func _on_value_changed(new_value: float) -> void:
+#     AudioServer.set_bus_volume_db(audio_bus, linear_to_db(new_value))
+#
+# func _validate_property(property: Dictionary):
+#     if property.name == "audio_bus":
+#         var busNumber = AudioServer.bus_count
+#         var options = ""
+#         for i in busNumber:
+#             if i > 0:
+#                 options += ","
+#             var busName = AudioServer.get_bus_name(i)
+#             options += busName
+#         property.hint_string = options
